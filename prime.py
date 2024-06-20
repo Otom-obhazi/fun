2c9246460eec6a460c778bed4493a9ca14cb8eed
Angel osim <69635048+Otom-obhazi@users.noreply.github.com>
2024-06-21 00:48:36 +0100

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,68 @@
+numm = int(input('Enter number from 1 - 20; '))
+
+if numm == 1:
+    print('prime numbers are greater than 1 and its not \
+     a product of two smaller natural number, so it is neither a \
+     prime number or not. ')
+
+elif numm == 2:
+    print('This is a prime number')
+
+elif numm == 3:
+    print('This is a prime number')
+
+elif numm == 4:
+    print('This is not a prime number')
+
+elif numm == 5:
+    print('This is a prime number')
+
+elif numm == 6:
+    print('This is not a prime number')
+
+elif numm == 7:
+    print('This is a prime number')
+
+elif numm == 8:
+    print('This is not a prime number')
+
+elif numm == 9:
+    print('This is not a prime number')
+
+elif numm == 10:
+    print('This is not a prime number')
+
+elif numm == 11:
+    print('This is a prime number')
+
+elif numm == 12:
+    print('This is not a prime number')
+
+elif numm == 13:
+    print('This is a prime number')
+
+elif numm == 14:
+    print('This is not a prime number')
+
+elif numm == 15:
+    print('This is not a prime number')
+
+elif numm == 16:
+    print('This is not a prime number')
+
+elif numm == 17:
+    print('This is a prime number')
+
+elif numm == 18:
+    print('This is not a prime number')
+
+elif numm == 19:
+    print('This is a prime number')
+
+elif numm == 20:
+    print('This is not a prime number')
+
+else:
+    print('Enter number from 1 - 20')
+
+# indicates prime numbers and none prime..
